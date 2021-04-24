8917ec0ac650600f2c36d0adc3a1b40b7ad15630
Bret Cadle <contactme@bretcadle.com>
2021-04-24 21:15:04 +0000

    initial

--- src/remove_minecraft.sh
+++ src/remove_minecraft.sh
@@ -1,4 +1,4 @@
-kubectl delete -n minecraft deployment minecraft-server
-kubectl delete -n minecraft service minecraft-server
-kubectl delete -n minecraft pvc minecraft-data-pvc
-kubectl delete -n minecraft pv minecraft-data
+kubectl delete -n minecraft deployment scramblecraft01-server
+kubectl delete -n minecraft service scramblecraft01-server
+kubectl delete -n minecraft pvc scramblecraft01-data-pvc
+kubectl delete -n minecraft pv scramblecraft01-data
